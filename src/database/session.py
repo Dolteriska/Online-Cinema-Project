00238c14a4d04bf7dacd58ed959d5230bb2998b3
Illia Savinov <dresar2404@gmail.com>
2026-06-24 12:43:24 +0200

    add basic infrastructure (alembic, SQLAlchemy, basic docker-compose for postgreSQL) and architecture of the future project

---
+++ src/database/session.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from src.config.settings import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
