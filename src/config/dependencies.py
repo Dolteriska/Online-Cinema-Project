14c2a572770d20c3db8238027e296f67a112a002
Illia Savinov <dresar2404@gmail.com>
2026-07-01 15:20:03 +0200

    add dependencies file to be able to use JWT auth manager

---
+++ src/config/dependencies.py
@@ -0,0 +1,26 @@
+from src.config.settings import Settings
+from src.security.interfaces import JWTAuthManagerInterface
+from src.security.token_manager import JWTAuthManager
+
+
+def get_jwt_auth_manager(settings: Settings) -> JWTAuthManagerInterface:
+    """
+    Create and return a JWT authentication manager instance.
+
+    This function uses the provided application settings to instantiate a JWTAuthManager, which implements
+    the JWTAuthManagerInterface. The manager is configured with secret keys for access and refresh tokens
+    as well as the JWT signing algorithm specified in the settings.
+
+    Args:
+        settings (BaseAppSettings, optional): The application settings instance.
+        Defaults to the output of get_settings().
+
+    Returns:
+        JWTAuthManagerInterface: An instance of JWTAuthManager configured with
+        the appropriate secret keys and algorithm.
+    """
+    return JWTAuthManager(
+        secret_key_access=settings.SECRET_KEY_ACCESS,
+        secret_key_refresh=settings.SECRET_KEY_REFRESH,
+        algorithm=settings.JWT_SIGNING_ALGORITHM
+    )
