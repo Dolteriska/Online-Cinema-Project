463f50a07d58abca2a72173d1c6dce1b6ab7426c
Illia Savinov <dresar2404@gmail.com>
2026-07-13 15:05:40 +0200

    add all models to __init__ file for future work with alembic

---
+++ src/database/models/__init__.py
@@ -0,0 +1,20 @@
+from src.database.models.users import (
+    UserGroupModel,
+    UserModel,
+    UserProfileModel,
+    ActivationTokenModel,
+    PasswordResetTokenModel,
+    RefreshTokenModel,
+)
+
+from src.database.models.movies import (
+    Genre,
+    Star,
+    Director,
+    Certification,
+    Movie,
+)
+
+from src.database.models.movie_interactions import (
+    FavoriteMovie,
+)
