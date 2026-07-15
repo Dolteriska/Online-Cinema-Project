49b1be05da8b38aac1b10b238a57df24baab1779
Illia Savinov <dresar2404@gmail.com>
2026-07-15 16:55:50 +0200

    add all routers to __init__ file to work with main.py file

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,2 +1,4 @@
-from src.routes.auth import router as auth_router
-from src.routes.admin import router as admin_router
+from src.routes.auth_router import router as auth_router
+from src.routes.admin_user_router import router as user_admin_router
+from src.routes.movies_router import router as movies_router
+from src.routes.admin_movie_router import router as movies_admin_router
