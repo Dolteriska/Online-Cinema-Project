0b6036030fd24df7afef1263177f151341afa219
Illia Savinov <dresar2404@gmail.com>
2026-07-20 12:13:01 +0200

    add new router to __init__ file

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,3 +2,4 @@ from src.routes.auth_router import router as auth_router
 from src.routes.admin_user_router import router as user_admin_router
 from src.routes.movies_router import router as movies_router
 from src.routes.admin_movie_router import router as movies_admin_router
+from src.routes.movie_and_user_interaction_routes import router as movie_and_user_interaction_router
