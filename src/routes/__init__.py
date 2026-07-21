e15e7c016274322420e0f4813086b955a9a7efae
Illia Savinov <dresar2404@gmail.com>
2026-07-21 10:43:13 +0200

    add profile_router and admin_profile_router to __init__.py

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -3,3 +3,5 @@ from src.routes.admin_user_router import router as user_admin_router
 from src.routes.movies_router import router as movies_router
 from src.routes.admin_movie_router import router as movies_admin_router
 from src.routes.movie_and_user_interaction_routes import router as movie_and_user_interaction_router
+from src.routes.user_profile_router import router as user_profile_router
+from src.routes.admin_user_profile_router import router as user_profile_admin_router
