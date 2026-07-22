b204b17af0e43cbbee95c5977e43365046377122
Illia Savinov <dresar2404@gmail.com>
2026-07-22 14:54:09 +0200

    add commentary router to __init__.py file

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -5,3 +5,4 @@ from src.routes.admin_movie_router import router as movies_admin_router
 from src.routes.movie_and_user_interaction_routes import router as movie_and_user_interaction_router
 from src.routes.user_profile_router import router as user_profile_router
 from src.routes.admin_user_profile_router import router as user_profile_admin_router
+from src.routes.movie_comments_router import router as commentary_router
