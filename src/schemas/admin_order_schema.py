36e66b03f1e387cc7d45d49ab3f5c2cb478e6593
Illia Savinov <dresar2404@gmail.com>
2026-07-28 16:07:07 +0200

    add 2 order schemas for admin order and basic orders

---
+++ src/schemas/admin_order_schema.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+from pydantic import BaseModel
+from datetime import datetime
+from src.database.models.orders import StatusEnum
+from decimal import Decimal
+
+from src.schemas.movies_schema import MovieShortResponseSchema
+from src.schemas.order_schema import OrderResponseSchema
+from src.schemas.users_schema import UserShortResponseSchema
+
+class AdminOrderResponseSchema(OrderResponseSchema):
+    user: Optional[UserShortResponseSchema]
+
+
+class AdminOrderListResponseSchema(BaseModel):
+    items: list[AdminOrderResponseSchema]
+    total: int
+    limit: int
+    offset: int
+    next: Optional[str] = None
+    previous: Optional[str] = None
+
+    model_config = {"from_attributes": True}
